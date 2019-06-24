4b33454261d26eac3635d61c92f04b824b48d77b
Natthapat Sotthisopha <natthapat.s@scale360solutions.com>
2019-06-24 15:59:55 +0700

    Add delay when attempting reconnecting for the first time

--- kurento-jsonrpc/kurento-jsonrpc-client/src/main/java/org/kurento/jsonrpc/client/AbstractJsonRpcClientWebSocket.java
+++ kurento-jsonrpc/kurento-jsonrpc-client/src/main/java/org/kurento/jsonrpc/client/AbstractJsonRpcClientWebSocket.java
@@ -607,7 +607,7 @@ public abstract class AbstractJsonRpcClientWebSocket extends JsonRpcClient {
   }
 
   private void reconnect(final String closeReason) {
-    reconnect(closeReason, 0, true);
+    reconnect(closeReason, RECONNECT_DELAY_TIME_MILLIS, true);
   }
 
   private void reconnect(final String closeReason, final long delayMillis,
